b6cc54a20392a8be607a2969ec074b9b9fd5fd99
Nishchal <gautam.nishchal@gmail.com>
2018-08-13 12:11:28 +0700

    WIP: saga listener

--- src/com/cyberhck/react/BaseAction.java
+++ src/com/cyberhck/react/BaseAction.java
@@ -1,26 +1,110 @@
 package com.cyberhck.react;
 
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.javascript.JSStubElementTypes;
+import com.intellij.lang.javascript.psi.ecma6.TypeScriptClass;
+import com.intellij.lang.javascript.psi.ecma6.impl.TypeScriptClassImpl;
+import com.intellij.lang.javascript.psi.ecmal4.JSClass;
+import com.intellij.lang.javascript.psi.ecmal4.JSReferenceList;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.PsiUtil;
+import org.jetbrains.annotations.Nullable;
 
 public abstract class BaseAction extends AnAction {
     @Override
     public void update(AnActionEvent e) {
         super.update(e);
-        Project project = e.getProject();
-        if (project == null) {
+        String fileType = this.getFileType(e);
+        if (fileType == null || !fileType.equals("TypeScript")) {
             this.hideAction(e);
             return;
         }
-        VirtualFile projectFile = project.getProjectFile();
-        if (projectFile == null) {
+        if (!this.actingFileType(e)) {
             this.hideAction(e);
             return;
         }
-        this.getLogClient("FILE_TYPE").info(projectFile.getFileType().getName());
     }
+
+    @Nullable
+    TypeScriptClass getTypeScriptClassFromCaret(AnActionEvent e) {
+        Caret caret = e.getData(PlatformDataKeys.CARET);
+        if (caret == null) {
+            return null;
+        }
+        PsiFile file = this.getPsiFile(e);
+        if (file == null) {
+            return null;
+        }
+        return PsiTreeUtil.getParentOfType(file.findElementAt(caret.getCaretModel().getOffset()), TypeScriptClass.class);
+    }
+
+    @Nullable
+    PsiFile getPsiFile(AnActionEvent e) {
+        VirtualFile file = this.getVirtualFile(e);
+        Project project = e.getProject();
+        if (file == null || project == null) {
+            return null;
+        }
+        return PsiUtil.getPsiFile(project, file);
+    }
+
+    @Nullable
+    String getFileType(AnActionEvent e) {
+        VirtualFile file = this.getVirtualFile(e);
+        if (file == null) {
+            return null;
+        }
+        return file.getFileType().getName();
+    }
+
+    @Nullable
+    VirtualFile getVirtualFile(AnActionEvent e) {
+        return e.getData(PlatformDataKeys.VIRTUAL_FILE);
+    }
+
+    abstract boolean actingFileType(AnActionEvent e);
+
+    boolean isTypeScriptClass(PsiFile file) {
+        ASTNode classNode = file.getNode().findChildByType(JSStubElementTypes.TYPESCRIPT_CLASS);
+        return classNode != null;
+    }
+
+    boolean doesClassExtend(TypeScriptClass typeScriptClass, String className) {
+        JSReferenceList extendedClasses = typeScriptClass.getExtendsList();
+        if (extendedClasses == null) {
+            return false;
+        }
+        JSClass[] cls = extendedClasses.getReferencedClasses();
+        if (cls.length == 0) {
+            return false;
+        }
+        for (JSClass cl : cls) {
+            String name = cl.getName();
+            if (name == null) {
+                continue;
+            }
+            if (name.equals(className)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    TypeScriptClassImpl getTypeScriptClass(PsiFile file) {
+        ASTNode classNode = file.getNode().findChildByType(JSStubElementTypes.TYPESCRIPT_CLASS);
+        if (classNode == null) {
+            return null;
+        }
+        return new TypeScriptClassImpl(classNode);
+    }
+
     Client getLogClient(String tag) {
         return new Client(tag, "http://localhost:9999/logs");
     }
--- src/com/cyberhck/react/SagaListenerMethodAction.java
+++ src/com/cyberhck/react/SagaListenerMethodAction.java
@@ -1,13 +1,86 @@
 package com.cyberhck.react;
 
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.javascript.JSElementTypes;
+import com.intellij.lang.javascript.JSTokenTypes;
+import com.intellij.lang.javascript.psi.JSFunction;
+import com.intellij.lang.javascript.psi.ecma6.TypeScriptClass;
+import com.intellij.lang.javascript.psi.ecma6.TypeScriptFunction;
+import com.intellij.lang.javascript.psi.impl.JSChangeUtil;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiParserFacade;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
 
 public class SagaListenerMethodAction extends BaseAction {
+    final private String baseClassName = "BaseSaga";
+    final private String listenerMethodName = "registerListeners";
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        // try to notify too.
-        // TODO: insert action logic here
-        this.getLogClient("ACTION_PERFORMED").info("action performed");
+        this.addListener(e);
+    }
+
+    private void addListener(AnActionEvent e) {
+        // first get the listener method and add listen action.
+        // then add the actual method later make it live action, for now hardcode
+        TypeScriptFunction listener = this.getListenerMethod(e);
+        Project project = e.getProject();
+        if (listener == null || project == null) {
+            return;
+        }
+        ASTNode node = this.getTakeLatest(project);
+        if (node == null) {
+            return;
+        }
+        // add child. but in other context.
+        CommandProcessor.getInstance().executeCommand(project, () -> {
+            ApplicationManager.getApplication().runWriteAction(() -> {
+                ASTNode block = listener.getNode().findChildByType(JSElementTypes.BLOCK_STATEMENT);
+                TypeScriptClass tsc = this.getTypeScriptClassFromCaret(e);
+                if (block == null || tsc == null) {
+                    return;
+                }
+                ASTNode rbrace = block.findChildByType(JSTokenTypes.RBRACE);
+                if (rbrace == null) {
+                    return;
+                }
+                JSFunction jsFun = (JSFunction) tsc;
+                listener.addBefore(node.getPsi(), rbrace.getPsi());
+//                listener.addBefore(node.getPsi(), rbrace.getPsi());
+            });
+        }, null, null);
+    }
+
+    PsiElement getWhitespace(@NotNull Project project) {
+        return PsiParserFacade.SERVICE.getInstance(project).createWhiteSpaceFromText("\n");
+    }
+    @Nullable
+    private ASTNode getTakeLatest(@NotNull Project project) {
+        return JSChangeUtil.createExpressionFromText(project, "takeLatest(SOMETHING, this.listen)");
+    }
+
+    @Nullable
+    private TypeScriptFunction getListenerMethod(AnActionEvent e) {
+        TypeScriptClass tsc = this.getTypeScriptClassFromCaret(e);
+        if (tsc == null) {
+            return null;
+        }
+        TypeScriptFunction[] listenerMethods = tsc.findFunctionsByName(listenerMethodName);
+        return listenerMethods[0];
+    }
+
+    @Override
+    boolean actingFileType(AnActionEvent e) {
+        TypeScriptClass typeScriptClass = this.getTypeScriptClassFromCaret(e);
+        if (typeScriptClass == null) {
+            return false;
+        }
+        return this.doesClassExtend(typeScriptClass, this.baseClassName);
     }
 }
